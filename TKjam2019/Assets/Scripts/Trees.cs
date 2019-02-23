5d66585d946d87d2fd5336978a232778b71c95cb
Emilian Szyburski <szybur@tlen.pl>
2019-02-23 02:25:54 +0100

    destroyed state

--- TKjam2019/Assets/Scripts/Trees.cs
+++ TKjam2019/Assets/Scripts/Trees.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 
 public class Trees : MonoBehaviour
 {
+
+    delegate void ProcessHit(RaycastHit hit);
+
+    void ProcessRayCast(ProcessHit processHit)
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(ray, out hit, 100))
+        {
+            processHit(hit);
+        }
+    }
+
+
     void Start()
     {
         InfectFirstTree();
@@ -11,7 +25,18 @@ public class Trees : MonoBehaviour
 
     void Update()
     {
-        
+        if (Input.GetMouseButtonUp(0))
+        {
+            ProcessRayCast((hit) =>
+            {
+                Tree tree = hit.collider.gameObject.GetComponent<Tree>();
+                if (tree != null)
+                {
+                    Debug.Log(tree.name);
+                    tree.MyState = Tree.State.DESTROYED;
+                }
+            });
+        }
     }
 
     void InfectFirstTree()
