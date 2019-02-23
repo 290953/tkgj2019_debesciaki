f14ee59010f64b934c2bf4dd7f6018a89da15698
Emilian Szyburski <szybur@tlen.pl>
2019-02-23 13:25:49 +0100

    water source and acid source

--- TKjam2019/Assets/Scripts/Player.cs
+++ TKjam2019/Assets/Scripts/Player.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    public int acidLoads = 5;
+    public int acidLoads = 0;
     public int waterLoads = 5;
+    public int maxLoads = 5;
 
     private Rigidbody rb;
     public float speed;
+    Collider collision;
 
     void Start()
     {
@@ -24,19 +26,87 @@ public class Player : MonoBehaviour
         rb.velocity = new Vector3(horizontal * speed, rb.velocity.y, vertical * speed);
     }
 
-    void OnTriggerStay(Collider col)
+    private void Update()
     {
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            HandleCollision(collision, KeyCode.E);
+        }
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            HandleCollision(collision, KeyCode.R);
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        collision = other;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        collision = null;
+    }
+
+    void HandleCollision(Collider col, KeyCode key)
+    {
+        if (col == null)
+        {
+            return;
+        }
         if (col.CompareTag("Tree"))
         {
-            Tree tree = col.GetComponent<Tree>();
-            if (Input.GetKey(KeyCode.E))
+            HandleTree(col, key);
+        }
+        else if (col.CompareTag("WaterSource"))
+        {
+            HandleWaterSource(col, key);
+        }
+        else if (col.CompareTag("AcidSource"))
+        {
+            HandleAcidSource(col, key);
+        }
+    }
+
+    void HandleWaterSource(Collider col, KeyCode key)
+    {
+        Source source = col.GetComponent<Source>();
+        if (key == KeyCode.E)
+        {
+
+            Debug.LogWarning("adding water");
+            if (acidLoads <= 0 && waterLoads < maxLoads)
             {
-                DestroyTree(tree);
+                waterLoads += source.GetLoad();
             }
-            if (Input.GetKey(KeyCode.R))
+        }
+    }
+
+    void HandleAcidSource(Collider col, KeyCode key)
+    {
+        Source source = col.GetComponent<Source>();
+        if (key == KeyCode.E)
+        {
+
+            Debug.LogWarning("adding acid");
+            if (waterLoads <= 0 && acidLoads < maxLoads)
             {
-                MakeMagicalTree(tree);
+                acidLoads += source.GetLoad();
             }
+        }
+    }
+
+
+    void HandleTree(Collider col, KeyCode key)
+    {
+        Tree tree = col.GetComponent<Tree>();
+        if (key == KeyCode.E)
+        {
+            DestroyTree(tree);
+        }
+        if (key == KeyCode.R)
+        {
+            MakeMagicalTree(tree);
         }
     }
 
